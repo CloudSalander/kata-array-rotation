1e35e58c16e3a3867503dea8bbe00857e6661106
Cloud <ruben.as532@gmail.com>
2023-12-27 01:23:32 +0100

    Apparently first complete solution

--- index.php
+++ index.php
@@ -1,37 +1,34 @@
 <?php
 /*TODO:
-Not working with n > array size. 
-Talk about pass by reference
+Do it with a single array?
 What if I want to do it to the right?
 */
 
 function rotateArrayByN(array $elements, int $n): array {
 	$new_indexes = calcNewIndexes($elements,$n);
-	print_r($new_indexes);
-	return $new_indexes;
+	$rotated_array = [];
+	foreach ($new_indexes as $key => $n) {	
+		$rotated_array[$n] = $elements[$key];
+		ksort($rotated_array);
+	}
+	return $rotated_array;
 }
 
 function calcNewIndexes(array $elements, int $n): array {
 	$new_indexes = [];
+	$normalized_n = $n % count($elements); 			//Prevent n greater than array length
 	foreach ($elements as $key => $element) {
-		$new_key = $key - $n;
-		if($new_key < 0) $new_key = count($elements) + ($new_key % (count($elements)));
+		$new_key = $key - $normalized_n;
+		if($new_key < 0) $new_key = count($elements) + $new_key; 
 		$new_indexes[] = $new_key;
 	}
-	foreach ($new_indexes as $key => $n) {
-			$aux = $elements[$n];
-			$elements[$n] = $elements[$key];
-			$elements[$key] = $elements[$n]; 
-	}
-	return $elements;
+	return $new_indexes;
 }
 
-
-
-
 $array = [1,2,3,4,5];
-rotateArrayByN($array,3);
-rotateArrayByN($array,1);
-rotateArrayByN($array,2);
+print_r(rotateArrayByN($array,51));
+print_r(rotateArrayByN($array,1));
+print_r(rotateArrayByN($array,2));
+print_r(rotateArrayByN($array,3));
 
 ?>
